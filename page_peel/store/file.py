55c71fdf00796fa376a6656fbec22cb47384eb22
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2016-07-17 20:14:09 -0300

    #2 Documentation.

--- page_peel/store/file.py
+++ page_peel/store/file.py
@@ -22,6 +22,44 @@ import os.path
 
 
 class FileStore(object):
+    """
+    ``FileStore`` is a store class that deals with files from a directory.
+
+    A store is an object which can retrieve content from a document, and save
+    new content to a document. A store document is usually an HTML document.
+    Where it is saved and how it is retrieved is defined by the store
+    implementation. We can have, for example, stores to manage documents from
+    the local filesystem, or manage documents via FTP or SSH etc. Each
+    document has an "address", which usually is an HTTP URL. It is up to the
+    store to map the URL to an editable document, if it is supposed to be
+    altered.
+
+    ``FileStore`` knows two things: a URL that points to a document, and a
+    directory where an identical document can be found::
+
+    >>> import tempfile
+    >>> tempdir = tempfile.mkdtemp()
+    >>> store = FileStore(
+    ...     base_address='http://test.com', directory=tempdir)
+
+    When we call ``FileStore.read()`` giving it a URL, it will remove the base
+    address from the given URL, and then try to find a file with whatever
+    remains::
+
+    >>> from inelegant.fs import temp_file
+    >>> with temp_file(dir=tempdir, name='index.html', content='Hello'):
+    ...     store.read('http://test.com/index.html')
+    'Hello'
+
+    We can also change it with ``write()``::
+
+    >>> with temp_file(dir=tempdir, name='index.html', content='Hello'):
+    ...     store.read('http://test.com/index.html')
+    ...     store.write('http://test.com/index.html', 'CHANGED')
+    ...     store.read('http://test.com/index.html')
+    'Hello'
+    'CHANGED'
+    """
 
     def __init__(self, base_address, directory):
         self.base_address = base_address
