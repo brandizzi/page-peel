0e94ffad415f9c9824e73844e5b4e365ae4687de
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2016-08-29 06:31:38 -0300

    #3 I could save it!

--- page_peel/page.py
+++ page_peel/page.py
@@ -2,6 +2,8 @@ import os.path
 import itertools
 import pkgutil
 
+import lxml.html
+
 import confeitaria.server
 import confeitaria.interfaces
 
@@ -11,15 +13,41 @@ class ServingPage(confeitaria.interfaces.Page):
     def __init__(self, store):
         self.store = store
 
-    def index(self):
+    def index(self, edit=False):
         request = self.get_request()
+
         try:
             path = os.path.join(*request.path_args)
         except TypeError:
             path = ''
 
-        return self.store.read(path)
+        content = self.store.read(path)
 
+        if edit:
+            editor_content = self.store.read('editor.html')
+
+            editor_doc = lxml.html.fromstring(editor_content)
+
+            textarea = editor_doc.find_class('doc-content')
+            if len(textarea) < 1:
+                raise NoElement(
+                    'Element textarea with class doc-content not found')
+
+            textarea[0].value = content
+
+            content = lxml.html.tostring(editor_doc)
+
+        return content
+
+    def action(self, content=None):
+        request = self.get_request()
+
+        try:
+            path = os.path.join(*request.path_args)
+        except TypeError:
+            path = ''
+
+        self.store.write(path, content)
 
 class Store(object):
 
@@ -35,6 +63,8 @@ class Store(object):
 
         return content
 
+    def write(self, path, content):
+        write_content_to_fs(self.root_dir, path, content)
 
 def get_content_from_fs(root_dir, path, default_file='index.html'):
     fs_path = os.path.join(root_dir, path)
@@ -56,6 +86,15 @@ def get_content_from_package(
             package_name, os.path.join(template_dir, path, 'index.html'))    
 
     return content
+
+def write_content_to_fs(root_dir, path, content, default_file='index.html'):
+    fs_path = os.path.join(root_dir, path)
+
+    if os.path.exists(fs_path) and os.path.isdir(fs_path):
+        fs_path = os.path.join(fs_path, default_file)
+
+    with open(fs_path, 'w') as f:
+        f.write(content)
 
 
 if __name__ == '__main__':
